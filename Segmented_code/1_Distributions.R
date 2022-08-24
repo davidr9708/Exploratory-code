596fec981b5cdd427b208ed509de68869cfd98c1
Daniel Rodriguez <71290957+davidr9708@users.noreply.github.com>
2022-08-24 17:40:36 -0500

    Add files via upload

--- Segmented_code/1_Distributions.R
+++ Segmented_code/1_Distributions.R
@@ -1,5 +1,7 @@
 # Distributions: This code chunk is to build functions 
-# to visualize the distributions of each variable
+#                to visualize the distributions of each variable
+library(pacman)
+p_load(MASS, tidyverse, caret, corrplot, mlbench, e1071, ggridges)
 
 ## Variables distribution
 ### This function will identify the type of each variable
@@ -9,13 +11,12 @@ variable.types <- function(data){
   numeric_variable   <- vector()
   factor_variable    <- vector()
   character_variable <- vector()
-  
+
   for(name in colnames(data)){
-    if(class(data[, name])[1] %in% c('ordered', 'factor')){
-      
+    if(class(data[name][[1]]) %in% c('ordered', 'factor')){
       factor_variable <- c(factor_variable, name)
     }
-    else if(class(data[, name])[1] %in% c('numeric', 'integer')){
+    else if(class(data[name][[1]]) %in% c('numeric', 'integer')){
       numeric_variable <- c(numeric_variable, name)
     }
     else{
@@ -33,17 +34,18 @@ variable.types <- function(data){
 ### of the numeric variables given
 plot.numeric <- function(data, numeric_variable, graph = 'hist'){
   
-  ifelse(length(numeric_variable) == 0,
-         message('Error: numeric_variable is empty'), 
-         skewed_values <- round(apply(data[numeric_variable], 2, skewness), 2))
+  if(length(numeric_variable) == 0){
+     message('Error: numeric_variable is empty')} 
   
+
   if(graph == 'both'){
     
     for(name in numeric_variable){
-      plot <- density(data[, name])
+      skewed_values <- round(skewness(data[,name][[1]], na.rm = T), 2)
+      plot <- density(data[, name][[1]], na.rm = T)
       par(mfrow=c(2,1))
-      hist(data[, name], xlab = name,
-           main = paste('Variable:', name,'\nSkweness:', skewed_values[name]))
+      hist(data[, name][[1]], xlab = name,
+           main = paste('Variable:', name,'\nSkweness:', skewed_values))
       
       plot(plot, main = NA)
       polygon(plot, col = 'darkblue')
@@ -52,16 +54,18 @@ plot.numeric <- function(data, numeric_variable, graph = 'hist'){
   else if(graph == 'hist'){
     
     for(name in numeric_variable){
-      hist(data[, name], xlab = name,
-           main =paste('Variable:', name,'\nSkweness:', skewed_values[name]))
+      skewed_values <- round(skewness(data[,name][[1]], na.rm = T), 2)
+      hist(data[, name][[1]], xlab = name,
+           main =paste('Variable:', name,'\nSkweness:', skewed_values))
     }
   }
   else if(graph == 'density'){
     
     for(name in numeric_variable){
-      plot <- density(data[, name])
+      skewed_values <- round(skewness(data[,name][[1]], na.rm = T), 2)
+      plot <- density(data[, name][[1]], na.rm = T)
       plot(plot,
-           main = paste('Variable:', name,'\nSkweness:', skewed_values[name]))
+           main = paste('Variable:', name,'\nSkweness:', skewed_values))
       polygon(plot, col = 'darkblue')
     }
   }
@@ -72,29 +76,30 @@ plot.numeric <- function(data, numeric_variable, graph = 'hist'){
 ### This function will plot the distribution
 ### of the nominal variables given
 plot.factors <- function(data, factor_variable){
-  
+
   for(name in factor_variable){
-    factor_levels <- levels(data[,name])
-    factor_length <- length(factor_levels) 
     
-    data <- data %>%
+    factor_levels <- levels(addNA(data[,name][[1]]))
+    factor_length <- length(factor_levels) 
+
+    data_summary <- data %>%
       mutate(total = n()) %>%
-      group_by_at(name) %>%
+      group_by_at(name,) %>%
       summarise(percent = round(n()/mean(total)*100, 1),
                 no_rows = n()) %>% 
-      ungroup() %>%
+      ungroup()  %>%
       mutate(maximum = max(no_rows),
              ratio = round(maximum/no_rows, 2),
              labels = paste0('n =', no_rows, '; ratio =', ' ', ratio),
              factors = fct_reorder(factor_levels, percent, .desc = FALSE))
     
-    plot <- data %>% 
+    plot <- data_summary %>% 
       ggplot(aes_string(x = 'percent', y = 'factors', label = 'labels')) +
-      geom_bar(stat = 'identity', fill =ifelse(data$percent < 10, 'darkred', 'darkgray'))  + 
-      geom_text(hjust = ifelse(data$percent <= 10, -0.2, 1.5 ),
-                color = ifelse(data$ratio >= 20, 'darkred', 
-                               ifelse(data$percent < 10, 'darkgray','white'))) +
-      geom_vline(xintercept = 10) +
+      geom_bar(stat = 'identity', fill =ifelse(data_summary$percent < 10, 'darkred', 'darkgray'))  + 
+      geom_text(hjust = ifelse(data_summary$percent <= 10, -0.2, 1.5 ),
+                color = ifelse(data_summary$ratio >= 20, 'darkred', 
+                               ifelse(data_summary$percent < 10, 'darkgray','white'))) +
+      geom_vline(xintercept = 10, linetype = 'dashed') +
       scale_x_continuous(expand = c(0,0)) +
       ggtitle(paste('Variable:', name, ';', 'Levels: ', factor_length)) + 
       xlab('Percentage (%)') +
