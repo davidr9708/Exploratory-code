7ea823b1a84ed6bc60ae75252ae3db8f43459986
Daniel Rodriguez <71290957+davidr9708@users.noreply.github.com>
2022-09-22 11:14:07 -0500

    Add files via upload

--- Segmented_code/2_Missing_values.R
+++ Segmented_code/2_Missing_values.R
@@ -1,5 +1,6 @@
 # Missing values: This code chunk will let you visualize the missing values
 
+library(tidyverse)
 ## Missing values in each column
 na.by.columns <- function(data, visual = 'graph'){
   
@@ -29,25 +30,37 @@ na.by.columns <- function(data, visual = 'graph'){
 
 ## Proportion of Missing values belonging to each class of the target
 target.proportion.na <- function(data, target, visual = 'graph'){
+  prop_sum <- cumsum(round(prop.table(table(data[target][1]))*100,2))
+  fill_order <- sort(names(prop_sum),decreasing = T)
+  l <- length(fill_order)
   
   predictor_na <- data %>%
+    rename('outcome' = target) %>%
     mutate_all(as.character) %>%
     gather(1:(ncol(data)-1), key = 'column', value = 'value') %>% 
     filter(is.na(value)) %>%
     group_by(column) %>%
     mutate(total_count = n()) %>%
-    group_by_at(.vars = c('column', target[1])) %>%
+    group_by_at(.vars = c('column', 'outcome')) %>%
     summarise(group_count = n(),
               na_percent  = group_count/mean(total_count)*100) %>%
-    mutate(column = fct_reorder(column, na_percent))
+    group_by_at('outcome') %>%
+    mutate(ranking = rank(na_percent),
+           column = fct_reorder(column, ranking, .desc = TRUE)) %>%
+    ungroup() %>%
+    mutate(outcome = factor(outcome, levels = fill_order)) 
   
   plot <- predictor_na %>% 
-    ggplot(aes_string(x = 'na_percent', y = 'column', label = 'group_count', fill = target)) +
+    ggplot(aes_string(x = 'na_percent', 
+                      y = 'column', 
+                      label = 'group_count', 
+                      fill = 'outcome')) +
     geom_bar(stat = 'identity') +
+    geom_vline(xintercept = prop_sum[1:l-1], linetype = 'dashed', color = 'black') +
     theme(plot.background  = element_blank(),
           panel.background = element_blank(),
           axis.title = element_blank(),
-          axis.ticks.y = element_blank())
+          axis.ticks.y = element_blank()) + scale_fill_brewer(palette = "Set2")
   
   if(visual == 'graph'){return(plot)}
   else if(visual == 'matrix'){return(predictor_na)}
@@ -66,15 +79,18 @@ na.by.target <- function(data, target, visual = 'graph'){
            Is_NA = ifelse(is.na(value), 'Yes', 'No')) %>%
     group_by_at(.vars = c('column', target[1], 'Is_NA')) %>%
     summarise(group_count = n(),
-              na_percent  = group_count/mean(total_count)*100) %>%
-    mutate(column = fct_reorder(column, na_percent))
-  
+              na_percent  = group_count/mean(total_count)*100) %>% 
+    group_by(Is_NA) %>%
+    mutate(ranking = rank(na_percent),
+      column = fct_reorder(column, ranking, .desc = TRUE))
   
   plot <- predictor_na_label %>% 
     ggplot(aes(x = na_percent, y = column, label = group_count, fill = Is_NA)) +
-    geom_bar(stat = 'identity') + facet_grid(reformulate(target))  + 
+    geom_bar(stat = 'identity') +
+    facet_grid(reformulate(target))  + 
     scale_fill_manual(values = c('gray','darkred')) + 
-    scale_x_continuous(breaks = c(50,100)) +
+    scale_x_continuous(breaks = c(10, 25, 50,100)) + 
+    labs(fill = "Missing data") +
     theme(plot.background  = element_blank(),
           panel.background = element_blank(),
           axis.title = element_blank(),
